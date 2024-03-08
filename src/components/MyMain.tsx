ac5258f68be96cd7b281c364553d5d9306a594a6
Omar Lo Giudice <146293874+FearXz@users.noreply.github.com>
2024-03-08 22:50:42 +0100

    ok

--- src/components/MyMain.tsx
+++ src/components/MyMain.tsx
@@ -1,12 +1,20 @@
-import React from "react";
-import { Col, Container, Row } from "react-bootstrap";
+import React, { useState } from "react";
+import { Button, Col, Container, Row } from "react-bootstrap";
 
 function MyMain() {
+  const [data, setData] = useState(false);
+
   return (
     <Container>
       <Row>
-        <Col className=" d-flex justify-content-center">
-          <div className=" fs-1 text-danger bg-darkgray">MEGA KEBAB</div>
+        {data && (
+          <Col xs={12} className=" d-flex justify-content-center">
+            <div className=" fs-1 text-danger bg-darkgray">MEGA KEBAB</div>
+          </Col>
+        )}
+
+        <Col xs={12} className=" d-flex justify-content-center mt-5">
+          <Button onClick={() => setData(true)}>Change State</Button>
         </Col>
       </Row>
     </Container>
